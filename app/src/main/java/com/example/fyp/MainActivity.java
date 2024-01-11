b00ca9de0515f63ce9f92c04145ceb7417c73959
iqbal <iqbaldaud16@gmail.com>
2024-01-12 00:40:36 +0800

    commit

--- app/src/main/java/com/example/fyp/MainActivity.java
+++ app/src/main/java/com/example/fyp/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
                 return false;
             }
-
+             ///asas
             @Override
             public boolean onQueryTextChange(String newText) {
                 return false;
